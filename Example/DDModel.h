85c8356e3f8cded4e6829dbbb3dc927f45eee144
dong <xiaoheiai4719@126.com>
2018-02-06 17:21:52 +0800

    first commit

---
+++ Example/DDModel.h
@@ -0,0 +1,15 @@
+//
+//  DDModel.h
+//  moduleA_Example
+//
+//  Created by dong on 2018/2/6.
+//  Copyright © 2018年 407615367@qq.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DDModel : NSObject
+
++ (void)testDDModel;
+
+@end
---
+++ Example/DDModel.m
@@ -0,0 +1,18 @@
+//
+//  DDModel.m
+//  moduleA_Example
+//
+//  Created by dong on 2018/2/6.
+//  Copyright © 2018年 407615367@qq.com. All rights reserved.
+//
+
+#import "DDModel.h"
+
+@implementation DDModel
+
++ (void)testDDModel{
+    NSLog(@"---------");
+}
+
+
+@end
